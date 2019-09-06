d0548cb46baa4a3389122cd4227e29d79b4b1d50
xiaosa <l3603v@163.com>
2019-09-06 21:54:02 +0800

    第一次提交

---
+++ git_test/src/git_demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package git_demo;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+
+
+    }
+}
