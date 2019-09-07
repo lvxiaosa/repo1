08c9967e901bc6c869136e2b52614da17f7d9bbd
xiaosa <l3603v@163.com>
2019-09-07 23:45:07 +0800

    这是一次测试 我并不清楚git的使用流程但是感觉好像没什么问题

---
+++ git_test2/src/cn/itcast/Nulls.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+public class Nulls {
+    public static void main(String[] args) {
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+        System.out.println("这是什么意思");
+    }
+}
